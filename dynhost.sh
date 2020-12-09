d434678d22369eaa0f62f7ac077edaca22a68d84
Alberto743 <albertop@aol.it>
2020-12-09 20:28:31 +0100

    Assure the portability of the shebang. Prevent curl hanging in case of lack of connectivity. Allow for an alternative fallback to retrieve current IPv4 if ifconfig.co is down.

--- dynhost.sh
+++ dynhost.sh
@@ -1,38 +1,30 @@
-#/bin/sh
+#!/usr/bin/env sh
 
-#
-# CONFIG
-#
-
+# Account configuration
 HOST=DOMAINE_NAME
 LOGIN=LOGIN
 PASSWORD=PASSWORD
 
-PATH_LOG=/var/log/dynhost
-CURRENT_DATE=`date`
+PATH_LOG=/var/log/dynhostovh.log
 
-#
-# GET IPs
-#
-
-HOST_IP=`dig +short $HOST`
-CURRENT_IP=`curl -4 ifconfig.co`
-
-#
-# DO THE WORK
-#
-if [ -z $CURRENT_IP ] || [ -z $HOST_IP ]
+# Get current IPv4 and corresponding configured
+HOST_IP=$(dig +short $HOST A)
+CURRENT_IP=$(curl -m 5 -4 ifconfig.co 2>/dev/null)
+if [ -z $CURRENT_IP ]
 then
-        echo "No IP retrieved" >> $PATH_LOG
-else
-        if [ "$HOST_IP" != "$CURRENT_IP" ]
-        then
-                echo "$CURRENT_DATE"": Current IP:" "$CURRENT_IP" "and" "host IP:" "$HOST_IP" "   IP has changed!" >> $PATH_LOG
-                RES=`curl --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP"`
-                echo "Result request dynHost:" >> $PATH_LOG
-                echo "$RES" >> $PATH_LOG
-        else
-                echo "$CURRENT_DATE"": Current IP:" "$CURRENT_IP" "and" "Host IP:" "$HOST_IP" "   IP has not changed" >> $PATH_LOG
-        fi
+  CURRENT_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 fi
 
+# Update dynamic IPv4, if needed
+if [ -z $CURRENT_IP ] || [ -z $HOST_IP ]
+then
+  echo "No IP retrieved" >> $PATH_LOG
+else
+  if [ "$HOST_IP" != "$CURRENT_IP" ]
+  then
+    echo "IP has changed" >> $PATH_LOG
+    RES=$(curl -m 5 --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP")
+    echo "Result request dynHost" >> $PATH_LOG
+    echo "$RES" >> $PATH_LOG
+  fi
+fi
